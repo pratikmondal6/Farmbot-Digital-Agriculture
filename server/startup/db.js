9019a4b52dec0803834cc41bc11739bfcfb76faf
David Schwar <davidschwaruni@gmail.com>
2025-05-14 14:29:23 +0200

    Creating model for plants in db.js

--- server/startup/db.js
+++ server/startup/db.js
@@ -11,3 +11,22 @@ module.exports = function () {
         .then(() => console.log("Connected to database..."))
         .catch(() => console.log("Cannot connect to database!"))
 }
+
+const plantscheme = new mongoose.scheme({ // creates scheme for plant
+  plant_type: {
+    type: String,
+    required: true, //If this attribute isn't given you will get an error 
+    unique: true, //Same plant_type can't be used twice
+    trim: true, //Automaticly deletes blank spaces at the beginning and the end
+  },
+  minimal_distance: {
+    type: Number,
+    required: true,
+  },
+            seeding_depth: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Plant', plantSchema); //creates actual modelout of the scheme
