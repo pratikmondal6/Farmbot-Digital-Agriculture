eb37baa187e2ad7c1b50511608a4f166d63bb040
Tarek <proch@rhrk.uni-kl.de>
2025-05-19 11:26:19 +0200

    ready for merge

--- server/startup/db.js
+++ server/startup/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true); // or false, depending on your preference
 
 module.exports = function () {
-<<<<<<< HEAD
     // the `strictQuery` option will be switched back to `false` by default in Mongoose 7. 
     // Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. 
     mongoose.set('strictQuery', false)
@@ -12,12 +11,3 @@ module.exports = function () {
         .then(() => console.log("Connected to database..."))
         .catch(() => console.log("Cannot connect to database!"))
 }
-=======
-  mongoose.connect('mongodb://localhost:27017/planttypes', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
-};
->>>>>>> b8b0405290688515b26becffa0794bc5edfed96c
