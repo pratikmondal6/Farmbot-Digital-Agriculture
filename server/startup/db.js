026f786eb692f80a276beb30e82ec9f5ac368575
Sina Tavakolifard <s.tavakolifard@gmail.com>
2025-05-19 11:25:30 +0200

    Corrected database links and updated connecting to it

--- server/startup/db.js
+++ server/startup/db.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true); // or false, depending on your preference
+const mongoose = require('mongoose')
+const config = require('config')
 
 module.exports = function () {
-  mongoose.connect('mongodb://localhost:27017/planttypes', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
-};
+    // the `strictQuery` option will be switched back to `false` by default in Mongoose 7. 
+    // Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. 
+    mongoose.set('strictQuery', false)
+
+    const db = config.get('db')
+    console.log(db)
+    mongoose.connect(db)
+        .then(() => console.log("Connected to database..."))
+        .catch(() => console.log("Cannot connect to database!"))
+}
