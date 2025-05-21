c18b9fe058b56904ed3da174aafd46300163e803
Maike Stoll <m_stoll19@cs.uni-kl.de>
2025-05-21 12:50:56 +0200

    added unit tests for seeding depth

--- server/index.js
+++ server/index.js
@@ -8,11 +8,14 @@ app.use(cookieParser());
 require("./startup/routes")(app);
 require("./startup/db")();
 require('dotenv').config();
-
 const plantRoutes = require('./routes/plantRoutes');
 app.use('/api/plant', plantRoutes);
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Listening on ${port}...`));
 
-module.exports = server;
+// Only start server if not required by another module (like in tests)
+if (require.main === module) {
+  app.listen(port, () => console.log(`Listening on ${port}...`));
+}
+
+module.exports = app;
