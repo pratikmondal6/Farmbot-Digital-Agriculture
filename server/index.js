5859738d23d81c9a2ff1430c0c0c2872932e6294
Sina Tavakolifard <s.tavakolifard@gmail.com>
2025-05-23 11:15:09 +0200

    save token into cookies and headers in the response of login

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Listening on ${port}...`));
 
 module.exports = server;
