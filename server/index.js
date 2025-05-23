8bee7e48bf31791936150dbb9ea914687b0773b9
Maike Stoll <m_stoll19@cs.uni-kl.de>
2025-05-23 10:27:32 +0200

    some small changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const plantRoutes = require('./routes/plantRoutes');
 app.use('/api/plant', plantRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Only start server if not required by another module (like in tests)
 if (require.main === module) {
