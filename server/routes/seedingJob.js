a942a5d94ef432c0e297f6727f7524b0f374ad78
Sina Tavakolifard <s.tavakolifard@gmail.com>
2025-06-13 20:39:37 +0200

    Seeding job backend completed

--- server/routes/seedingJob.js
+++ server/routes/seedingJob.js
@@ -1,31 +1,11 @@
-const bcrypt = require('bcrypt')
 const express = require("express");
 const router = express.Router();
-const { User } = require("../models/users");
-const axios = require('axios');
+const { Seed } = require("../models/seed");
+const { Farmbot } =  require("farmbot");
 
-const sleep = (ms)=> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+const move = async (bot, x, y, z) => {
+  await bot.moveAbsolute({ x: x, y: y, z: z, speed: 100 });
 }
-
-const move = async (auth_token, position) => {
-  // Go to a little higher from seeder object
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer YOUR_TOKEN_HERE',
-    'Auth-Token': auth_token
-  };
-  
-  await axios.post("http://localhost:5000/move", position, { headers })
-    .then(response => {
-      console.log('Response:', response.data);
-    })
-    .catch(error => {
-      console.error('Error:', error.response ? error.response.data : error.message);
-    });
-  await sleep(1000)
-}
-
 
 router.post("/start", async (req, res) => {
   if (!req.headers["auth-token"]) {
@@ -35,48 +15,86 @@ router.post("/start", async (req, res) => {
     })
   }
 
+  if (req.body.x === undefined || req.body.y === undefined) {
+    return res.status(500).send({
+      "status": 500,
+      "message": "x and y is not sent in body"
+    })
+  }
+
+  const token = req.headers["auth-token"];
+  let bot = new Farmbot({ token: token });
+  await bot.connect()
+
   // Go to higher than seeder object
-  await move(req.headers["auth-token"], {x: 2630, y: 245, z:-395})
+  await move(bot, x=2630, y=245, z=-395)
 
   // Go down to get seeder object
-  await move(req.headers["auth-token"], {x: 2630, y: 245, z:-410})
+  await move(bot, x=2630, y=245, z=-410)
 
   // Go a little outside to take it out
-  await move(req.headers["auth-token"], {x: 2500, y: 245, z:-410})
+  await move(bot, x=2500, y=245, z=-410)
 
   // Go to a little higher from seed
-  await move(req.headers["auth-token"], {x: 2130, y: 25, z:-410})
-
-  // Go down to get seed
-  await move(req.headers["auth-token"], {x: 2130, y: 25, z:-520})
+  await move(bot, x=2130, y=25, z=-410)
 
   // Start suction
+  await bot.writePin({
+    pin_number: 9,
+    pin_value: 1,          // 1 = ON (HIGH), 0 = OFF (LOW)
+    pin_mode: 0            // 0 = digital, 1 = analog
+  });
+
+  // Go down to get seed
+  await move(bot, x=2130, y=25, z=-520)
 
   // Go higher
-  await move(req.headers["auth-token"], {x: 2130, y: 25, z:-480})
+  await move(bot, x=2130, y=25, z=-480)
 
   // Go to location of seeding
-  await move(req.headers["auth-token"], {x: req.body.x, y: req.body.y, z:-480})
+  await move(bot, x=req.body.x, y=req.body.y, z=-480)
 
   // Go down and seed
-  await move(req.headers["auth-token"], {x: req.body.x, y: req.body.y, z:-520 - req.body.z})
+  await move(bot, x=req.body.x, y=req.body.y, z=-520 - req.body.z)
 
   // Stop suction
+  await bot.writePin({
+    pin_number: 9,
+    pin_value: 0,          // 1 = ON (HIGH), 0 = OFF (LOW)
+    pin_mode: 0            // 0 = digital, 1 = analog
+  });
 
   // Go up
-  await move(req.headers["auth-token"], {x: req.body.x, y: req.body.y, z:-480})
+  await move(bot, x=req.body.x, y=req.body.y, z=-480)
+
+  const seed = new Seed({
+    seed_name: req.body.seed_name ? req.body.seed_name : "random_seed",
+    seeding_date: req.body.seeding_date ? req.body.seeding_date : Date.now(),
+    x: req.body.x,
+    y: req.body.y,
+    z: req.body.z,
+  });
+
+  await seed.save();
 
   // Go to left of seeder position
-  await move(req.headers["auth-token"], {x: 2500, y: 245, z:-410})
+  await move(bot, x=2500, y=245, z=-410)
 
   // Go to seeder position
-  await move(req.headers["auth-token"], {x: 2630, y: 245, z:-410})
+  await move(bot, x=2630, y=245, z=-410)
 
   // Go up to release it
-  await move(req.headers["auth-token"], {x: 2630, y: 245, z:-395})
+  await move(bot, x=2630, y=245, z=-395)
 
-  res.send({"message": "okay"})
+  res.status(200).send({
+    "message": "Seed is planted successfully"
+  })
 });
+
+router.get("/seeds", async (req, res) => {
+  const seeds = await Seed.find({});
+  res.status(200).send(seeds)
+})
 
 
 module.exports = router;
